f5cec78636211f73f614ab6304f50874ec362a15
Amélie Reynaud <239321608+AReyn343@users.noreply.github.com>
2026-02-24 14:06:58 +0100

    Minor fixes

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  console.log("someone hit the root endpoint")
-  res.json({ message: "Welcome to the Enhanced Express Todo App!" });
+  res.json({ message: "Welcome to the Noble To Do App!" });
 });
 
 app.use("/todos", todoRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 module.exports = app;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const initSqlJs = require("sql.js");
 const fs = require("fs");
 const path = require("path");
 
-const DB_PATH = path.join(__dirname, "..", process.env.DB_PATH || "todo.db");
+const DB_PATH = path.join(__dirname, "..", process.env.DB_PATH);
 
 let db;
 
