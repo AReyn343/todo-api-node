5b2e91ea2c064d46bddbb5a74840ca743634e08a
Amélie Reynaud <239321608+AReyn343@users.noreply.github.com>
2026-02-25 11:56:08 +0100

    Edit database.js to cover test with .env untracked

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const initSqlJs = require("sql.js");
 const fs = require("fs");
 const path = require("path");
 
-const DB_PATH = path.join(__dirname, "..", process.env.DB_PATH);
+const DB_PATH = path.join(__dirname, "..", process.env.DB_PATH || "todo.db");
 
 let db;
 
